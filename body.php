55437182e6e5f28a93a27b89360f258562bd9835
Obez99 <giuliogx@live.it>
2021-12-01 15:58:31 +0100

    Styles header

---
+++ body.php
@@ -0,0 +1,9 @@
+<body>
+  <header>
+    <img src="img/logo.png" alt="logo">
+  </header>
+
+  <body>
+
+  </body>
+</body>
---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+require_once "head.php";
+require_once "body.php";
+?>
+
+</html>
