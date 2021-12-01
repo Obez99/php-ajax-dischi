ef760c5ba75a5227de6243d52c7df72570746b1c
Obez99 <giuliogx@live.it>
2021-12-01 16:22:23 +0100

    Creates single card

--- body.php
+++ body.php
@@ -3,7 +3,19 @@
     <img src="img/logo.png" alt="logo">
   </header>
 
-  <body>
+  <main>
+    <div class="container">
+      <div class="row row-cols-5">
+        <div class="col">
+          <div class="cd-card">
+            <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+            <h4>NEW JERSEY</h4>
+            <span>Bon Jovi</span>
+            <span>1988</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 
-  </body>
 </body>
